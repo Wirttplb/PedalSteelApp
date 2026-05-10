c7f0577c8a02a6de9b1754ea8a17ba214f52d347
Romain Fournet <romain-fournet@hotmail.fr>
2026-05-09 21:27:36 -0400

    Added some settings back

--- app/settings.tsx
+++ app/settings.tsx
@@ -5,6 +5,7 @@ import { Dimensions, Pressable, StyleSheet, Text, View, ScrollView } from 'react
 import RNPickerSelect from 'react-native-picker-select';
 import Neck from '../components/neck';
 import { useKey } from './keyContext';
+import { Pedal } from '../fretboardEngine/pedal';
 
 export default function SettingsScreen() {
     const router = useRouter();
@@ -79,6 +80,97 @@ export default function SettingsScreen() {
                                 { label: 'Standard', value: 'Standard' },
                             ]}
                             value={tuning}
+                            style={pickerStyles}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.dropdownRow}>
+                    <View style={styles.dropdownWrapper}>
+                        <RNPickerSelect
+                            placeholder={{}}
+                            useNativeAndroidPickerStyle ={false}
+                            onValueChange={(itemValue) => {setSelectedKey(itemValue);}}
+                            items={[
+                                { label: 'C', value: 'C' },
+                                { label: 'C#', value: 'C#' },
+                                { label: 'D', value: 'D' },
+                                { label: 'D#', value: 'D#' },
+                                { label: 'E', value: 'E' },
+                                { label: 'F', value: 'F' },
+                                { label: 'F#', value: 'F#' },
+                                { label: 'G', value: 'G' },
+                                { label: 'G#', value: 'G#' },
+                                { label: 'A', value: 'A' },
+                                { label: 'A#', value: 'A#' },
+                                { label: 'B', value: 'B' },
+                            ]}
+                            value={selectedKey}
+                            style={pickerStyles}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.dropdownRow}>
+                    <View style={[styles.dropdownWrapper, {opacity: chordMode === 'Scale' ? 1 : 0.5}]}>
+                        <RNPickerSelect
+                            placeholder={{}}
+                            useNativeAndroidPickerStyle ={false}
+                            onValueChange={(itemValue) => setSelectedMode(itemValue)}
+                            items={[
+                                { label: 'Major', value: 'Major' },
+                                { label: 'Minor', value: 'Minor' },
+                                { label: 'Major Pentatonic', value: 'Major Pentatonic' },
+                                { label: 'Minor Pentatonic', value: 'Minor Pentatonic' },
+                                { label: 'Dorian', value: 'Dorian' },
+                                { label: 'Phrygian', value: 'Phrygian' },
+                                { label: 'Lydian', value: 'Lydian' },
+                                { label: 'Mixolydian', value: 'Mixolydian' },
+                                { label: 'Aeolian', value: 'Aeolian' },
+                                { label: 'Locrian', value: 'Locrian' },
+                                { label: 'Diminished Seventh', value: 'Diminished Seventh' },
+                                { label: 'Whole-tone', value: 'Whole-tone' },
+                            ]}
+                            value={selectedMode}
+                            style={pickerStyles}
+                        />
+                    </View>
+                    <View style={[styles.dropdownWrapper, {opacity: chordMode === 'Chord' ? 1 : 0.5}]}>
+                        <RNPickerSelect
+                            placeholder={{}}
+                            useNativeAndroidPickerStyle ={false}
+                            onValueChange={(itemValue) => {setChordType(itemValue);}}
+                            items={[
+                                { label: 'M', value: 'M' },
+                                { label: 'm', value: 'm' },
+                                { label: 'M7', value: 'M7' },
+                                { label: 'm7', value: 'm7' },
+                                { label: '7', value: '7' },
+                                { label: 'aug', value: 'aug' },
+                                { label: 'dim', value: 'dim' },
+                                { label: 'sus2', value: 'sus2' },
+                                { label: 'sus4', value: 'sus4' },
+                                { label: 'M6', value: 'M6' },
+                                { label: 'mm6', value: 'mm6' },
+                                { label: 'mM6', value: 'mM6' },
+                                { label: 'M7/6', value: 'M7/6' },
+                                { label: 'M6add9', value: 'M6add9' },
+                                { label: 'add9', value: 'add9' },
+                                { label: 'madd9', value: 'madd9' },
+                                { label: 'M9', value: 'M9' },
+                                { label: 'm9', value: 'm9' },
+                                { label: 'mM9', value: 'mM9' },
+                                { label: '9', value: '9' },
+                                { label: '7b9', value: '7b9' },
+                                { label: '7#9', value: '7#9' },
+                                { label: '11', value: '11' },
+                                { label: '13', value: '13' },
+                                { label: 'Mb5', value: 'Mb5' },
+                                { label: 'm7b5', value: 'm7b5' },
+                                { label: 'mb5bb7', value: 'mb5bb7' },
+                                { label: 'b5b13', value: 'b5b13' },
+                            ]}
+                            value={chordType}
                             style={pickerStyles}
                         />
                     </View>
